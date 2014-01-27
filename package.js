882eda1e7516c0f515e125681775409a7699ff11
Ryan Yeske <rcyeske@gmail.com>
2014-01-26 23:25:19 -0700

    add smart.json

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  summary: "Simple site builder based on iron-router"
+  summary: "Simple site builder"
 });
 
 Package.on_use(function (api, where) {
@@ -10,9 +10,3 @@ Package.on_use(function (api, where) {
   api.add_files('lib/client/router.js', 'client');
   api.add_files('lib/client/templates.html', 'client');
 });
-
-// Package.on_test(function (api, where) {
-//   api.use(['tinytest', 'test-helpers'], ['client', 'server']);
-//   // api.add_files([''], ['client', 'server']);
-//   // api.export([''], ['client', 'server']);
-// });
